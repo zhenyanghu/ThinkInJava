ccbe955aec8770b16597a55640cca902c7539a7f
zhenyanghu <huzhenyoung@163.com>
2019-01-08 23:03:38 +0800

    IDEA自动导包确实好用

--- src/othertest/TestGit.java
+++ src/othertest/TestGit.java
@@ -1,6 +1,8 @@
 package othertest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Rocky on 2019/1/8.
@@ -9,5 +11,7 @@ public class TestGit {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date);
+        List<String> list = new ArrayList<>();
+        System.out.println("自动导包确实好用");
     }
 }
