bb83c0521702c4a66d909ecc8ba5f832a9652866
zhenyanghu <huzhenyoung@163.com>
2019-01-08 22:53:40 +0800

    test git

---
+++ src/othertest/TestGit.java
@@ -0,0 +1,13 @@
+package othertest;
+
+import java.util.Date;
+
+/**
+ * Created by Rocky on 2019/1/8.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
